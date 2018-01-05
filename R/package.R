6ed6485f713f1930f5c20a42d599e098370306f3
Herb Susmann <herbps10@gmail.com>
2018-01-05 15:14:17 -0500

    Importing package files

---
+++ R/package.R
@@ -0,0 +1,109 @@
+#' Instruction
+#' 
+#' A data frame on school instruction results.
+#' 
+#' 
+#' @name instruction
+#' @docType data
+#' @format A data frame with 1190 observations on the following 13 variables.
+#' \describe{ \item{list("X")}{a numeric vector} \item{list("girl")}{a numeric
+#' vector} \item{list("minority")}{a numeric vector} \item{list("mathkind")}{a
+#' numeric vector} \item{list("mathgain")}{a numeric vector}
+#' \item{list("ses")}{a numeric vector} \item{list("yearstea")}{a numeric
+#' vector} \item{list("mathknow")}{a numeric vector} \item{list("housepov")}{a
+#' numeric vector} \item{list("mathprep")}{a numeric vector}
+#' \item{list("classid")}{a numeric vector identifying the class within school}
+#' \item{list("schoolid")}{a numeric vector identifying the school}
+#' \item{list("childid")}{a numeric vector} }
+#' @source West, B., Welch, K. B., & Galecki, A. T. (2006). Linear mixed
+#' models: a practical guide using statistical software. Chapman & Hall/CRC.
+#' @keywords datasets
+#' @examples
+#' 
+#' # The following code takes a few minutes to run.
+#' # In the interest of saving CRAN's example testing time,
+#' # it has been commented out. If you want to use it,
+#' # just uncomment and run.
+#' 
+#' # data(instruction)
+#' # attach(instruction)
+#' 
+#' # data = data.frame(
+#' #   y = mathgain,
+#' #   mathkind = mathkind, 
+#' #   girl = girl,
+#' #   minority = minority,
+#' #   ses = ses, 
+#' #   school = factor(schoolid), 
+#' #   section = factor(classid))
+#' 
+#' 
+#' # fit.rlme = rlme(y ~ 1 + mathkind + girl + minority + ses + (1 | school) + (1 | school:section),
+#' #  data = data,
+#' #  method = "gr")
+#'   
+#' # summary(fit.rlme)
+#' 
+#' @export
+NULL
+
+#' rlme
+#' 
+#' An R package for rank-based robust estimation and prediction in random
+#' effects nested models
+#' 
+#' \tabular{ll}{ Package: \tab rlme\cr Type: \tab Package\cr Version: \tab
+#' 0.2\cr Date: \tab 2013-07-07\cr License: \tab GPL (>= 2)\cr }
+#' 
+#' @name rlme-package
+#' @docType package
+#' @author Yusuf Bilgic \email{bilgic@@geneseo.edu}, Herb Susmann
+#' \email{hps1@@geneseo.edu} and Joseph McKean \email{joemckean@@yahoo.com}
+#' 
+#' Maintainer: Yusuf Bilgic \email{bilgic@@geneseo.edu} or
+#' \email{yusuf.k.bilgic@@gmail.com}
+#' @seealso \code{\link{rlme}}
+#' @keywords models package
+#' @examples
+#' 
+#' 
+#' library(rlme)
+#' data(schools)
+#' formula = y ~ 1 + sex + age + (1 | region) + (1 | region:school)
+#' rlme.fit = rlme(formula, schools)
+#' summary(rlme.fit)
+#' 
+NULL
+
+#' PISA Literacy Data
+#' 
+#' The data in Program for International Assessment (PISA) on academic
+#' proficiency in schools around the world.
+#' 
+#' 
+#' @name schools
+#' @docType data
+#' @format A data frame with 334 observations on the following 6 variables.
+#' \describe{ \item{list("y")}{a numeric vector indicating student literacy}
+#' \item{list("socio")}{a numeric vector} \item{list("sex")}{a numeric vector}
+#' \item{list("age")}{a numeric vector} \item{list("region")}{a numeric vector
+#' indicating four regions} \item{list("school")}{a numeric vector indicating
+#' the schools within region} }
+#' @references OECD (2010). PISA 2009 Results. http://www.oecd.org/ %% ~~
+#' possibly secondary sources and usages ~~
+#' @keywords datasets
+#' @examples
+#' 
+#' #
+#' # The example takes a few seconds to run, so in order to 
+#' # save CRAN's testing time it has been commented out. 
+#' # To run, simply uncomment and execute.
+#' #
+#' 
+#' # data(schools)
+#' # rlme.fit = rlme(y ~ 1 + sex + age + (1 | region) + (1 | region:school), 
+#' #	schools, method="gr")
+#' # summary(rlme.fit)
+#' 
+#' @export
+NULL
